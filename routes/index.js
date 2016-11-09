6f05dbb63a4b9695e7c4df9622a43ffdafb0b083
Noah Frank <udadaadaa@gmail.com>
2016-11-08 20:04:01 -0500

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const router = express.Router();
+
+const datamuse = require('datamuse');
+const syllable = require('syllable');
+const _ = require('lodash');
+
+
+const SIMILAR_SCORE_CONSTANT = 500;
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    modifyText(`They put me in the oven to bake.
+Me a deprived and miserable cake.
+Feeling the heat I started to bubble.
+Watching the others I knew I was in trouble...`,
+        (result) => {
+            console.log(result);
+    });
+    res.render('index', { title: 'Express' });
+});
+
+function modifyText(text, cb) {
+    let lines = _.split(text.trim(), '\n');
+    let done = [];
+    for (let i = 0; i < lines.length; i++) {
+        let line = lines[i];
+        let words = _.split(line, ' ');
+        let lastWord = words[words.length - 1].replace(/[^\w]/g,''); // Want to rhyme last word
+        getRhymeFor(lastWord, (word) => {
+            // Now rejoin line and replace for final text
+            console.log("Found rhyme for", lastWord, ":", word, "at index:", i);
+            words[words.length - 1] = word;
+            lines[i] = _.join(words, ' ');
+            done.push(i);
+            if (done.length == lines.length) {
+                cb(_.join(lines, '\n'));
+            }
+        });
+    }
+}
+
+function getRhymeFor(word, cb) {
+    let wordSyl = syllable(word);
+    datamuse.words({
+        rel_rhy: word
+    }).then((json) => {
+        let sameSylRhymes = _.filter(json, (word) => {
+            return word.numSyllables == wordSyl &&
+                   word.score >= SIMILAR_SCORE_CONSTANT;
+        });
+        let randWordIndex = getRandomInt(0, sameSylRhymes.length);
+        cb(sameSylRhymes[randWordIndex].word);
+    }).catch(error => {
+        console.log(error.response.body);
+        console.log("ERROR occured with rhyme request");
+        // TODO make server report error in api request, like api is down so we can't work right now
+        //=> 'Internal server error ...'
+    });
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+module.exports = router;
