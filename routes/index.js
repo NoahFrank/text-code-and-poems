3f07bcaa0f7460e96014addb78702f3fb7564e46
Noah Frank <udadaadaa@gmail.com>
2016-11-09 01:05:13 -0500

    -Added logic for if a word has no rhymes, not fully implemented but will send false down the line -Fixed bug where duplicates were not being removed

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ const SIMILAR_SCORE_CONSTANT = 500;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    modifyText(`They put me in the oven to bake.
+    let test = new Poem(`They put me in the oven to bake.
 Me a deprived and miserable cake.
 Feeling the heat I started to bubble.
 Watching the others I knew I was in trouble...`,
@@ -20,20 +20,12 @@ Watching the others I knew I was in trouble...`,
     res.render('index', { title: 'Express' });
 });
 
-
-
-
 class Poem {
 
     constructor(text, cb) {
-        this.modifiedText = "";
-        this.wordsDone = [];
+        this.wordsDone = []; // Keeps track of rhyming words we have found to prevent duplicates
 
         this.modifyText(text, cb);
-    }
-
-    getModifiedText() {
-        return this.modifiedText;
     }
 
     modifyText(text, cb) {
@@ -60,22 +52,46 @@ class Poem {
         datamuse.words({
             rel_rhy: word
         }).then((json) => {
-            let sameSylRhymes = _.filter(json, (word) => {
-                return word.numSyllables == wordSyl &&
-                    word.score >= SIMILAR_SCORE_CONSTANT;
-            });
-            // Don't want to pick the same word we have already gotten
-            _.forEach(this.wordsDone, (removeWord) => {
-                _.remove(sameSylRhymes, (word) => removeWord === word);
-            });
-            let randWordIndex = getRandomInt(0, sameSylRhymes.length);
-            cb(sameSylRhymes[randWordIndex].word);
+            let sameSylRhymes = this.filterRhymes(json, wordSyl);
+            // Determine if we actually found a rhyme
+            if (sameSylRhymes) {
+                let randWordIndex = getRandomInt(0, sameSylRhymes.length);
+                cb(sameSylRhymes[randWordIndex].word);
+            } else {
+                // TODO: failed to get rhyme for given word (over SIMILAR_SCORE_CONSTANT)
+            }
         }).catch(error => {
             console.log(error.response.body);
             console.log("ERROR occured with rhyme request");
             // TODO make server report error in api request, like api is down so we can't work right now
             //=> 'Internal server error ...'
         });
+    }
+
+    filterRhymes(rhymes, wordSyl, syllable=true) {
+        let sameSylRhymes = _.filter(rhymes, (word) => {
+            if (syllable) {
+                return word.numSyllables == wordSyl &&
+                    word.score >= SIMILAR_SCORE_CONSTANT;
+            } else {
+                return word.score >= SIMILAR_SCORE_CONSTANT;
+            }
+        });
+        // TODO: Handle if no words fit criteria, or if a couple meet and they are duplicates
+        // Don't want to pick the same word we have already gotten
+        _.forEach(this.wordsDone, (removeWord) => {
+            sameSylRhymes = _.remove(sameSylRhymes, (rhymeWord) => removeWord === rhymeWord);
+        });
+        // Logic for no rhymes, try without same syllable, then fail
+        if (sameSylRhymes.length == 0) {
+            if (syllable) {
+                filterRhymes(rhymes, wordSyl, false);
+            } else {
+                return false;
+            }
+        }
+
+        return sameSylRhymes;
     }
 }
 
